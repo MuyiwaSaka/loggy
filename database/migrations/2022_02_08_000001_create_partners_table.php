e8a153bd441aac570f2340929f3f8741f67d7d39
olumuyiwa.saka <sakamuyiwa@gmail.com>
2022-02-08 23:17:28 +0100

    Proper tailwind config and setup

---
+++ database/migrations/2022_02_08_000001_create_partners_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->id();
+            $table->string('companyname');
+            $table->string('companyemail')->unique();
+            $table->string('companyaddress')->nullable();
+            $table->string('preferredpickup')->nullable();
+            $table->string('pickupgeolocation')->nullable();
+            $table->string('phone');
+            $table->string('website')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partners');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/app.css" rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
-    <nav>
+<body  class="bg-gray-200">
+    <nav  class="p-6 bg-white flex justify-between mb-6">
         <ul>
-            <li><a href="">Home</a></li>
+            <li><a href="" class='p-3'>Home</a></li>
             @guest
                 <li><a href="">Become A Partner</a></li>    
             @endguest
